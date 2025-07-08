6222b6823ad3ca036d58f52379ce335c4e7ecbd3
hyeonil98 <lhi0711@naver.com>
2025-07-08 17:58:39 +0900

    2025.07.08 다가구주택 수집 로직 추가

--- db_utils.py
+++ db_utils.py
@@ -1,14 +1,18 @@
-from sqlalchemy import create_engine
+from _ast import Dict
+from typing import List
+
+from sqlalchemy import create_engine, inspect, or_, and_, select
 from sqlalchemy.dialects.mysql import insert
+from sqlalchemy.orm import Session
+from typing_extensions import Any
 
-from models import Base, RegionCode
-
+from models import Base, RegionCode, RealEstateDeal
 
 
 def initialize_engine(echo=False, create_schema=False):
     # 아이디 비밀번호 secret 으로 설정
     engine = create_engine(
-        "mysql+pymysql://:!@database-1.cleosac8ya6g.ap-northeast-2.rds.amazonaws.com:3306/estatedb?charset=utf8mb4",
+        "mysql+pymysql://:@database-1.cleosac8ya6g.ap-northeast-2.rds.amazonaws.com:3306/estatedb?charset=utf8mb4",
         echo=echo,
         pool_size=5,
         max_overflow=2,
@@ -23,6 +27,48 @@ def initialize_engine(echo=False, create_schema=False):
 # 최초 1회만 True
 engine = initialize_engine(create_schema=False)
 
+def fetch_filtered_table_data_with_like_multi_like(
+    table_name,
+    filters,
+    like_filters):
+    """
+    LIKE 조건에 리스트를 허용하여 서울%, 경기% 등 복수 조건 검색 지원
+    """
+    inspector = inspect(engine)
+    session = Session(bind=engine)
+
+    if table_name not in inspector.get_table_names():
+        raise ValueError(f"'{table_name}' 테이블이 존재하지 않습니다.")
+
+    table = Base.metadata.tables.get(table_name)
+    if table is None:
+        raise ValueError(f"'{table_name}' 테이블은 Base에 정의되어 있지 않습니다.")
+
+    try:
+        conditions = []
+
+        # 일반 조건
+        for key, value in filters.items():
+            if key in table.c:
+                conditions.append(table.c[key] == value)
+            else:
+                raise ValueError(f"'{key}' 컬럼이 없습니다.")
+
+        # LIKE 조건
+        if like_filters:
+            for key, patterns in like_filters.items():
+                if key in table.c:
+                    like_conds = [table.c[key].like(p) for p in patterns]
+                    conditions.append(or_(*like_conds))
+                else:
+                    raise ValueError(f"'{key}' 컬럼이 없습니다.")
+
+        stmt = select(table).where(and_(*conditions))
+        result = session.execute(stmt).mappings().all()
+        return result
+
+    finally:
+        session.close()
 
 def upsert_post(data):
     stmt = insert(RegionCode).values(**data)
@@ -39,6 +85,20 @@ def upsert_post(data):
         locathigh_cd=stmt.inserted.locathigh_cd,
         locallow_nm=stmt.inserted.locallow_nm,
         adpt_de=stmt.inserted.adpt_de
+    )
+    with engine.begin() as conn:
+        conn.execute(update_stmt)
+
+def upsert_real_estate(data):
+    stmt = insert(RealEstateDeal).values(**data)
+    update_stmt = stmt.on_duplicate_key_update(
+        mhouse_nm=stmt.inserted.mhouse_nm,
+        monthly_rent=stmt.inserted.monthly_rent,
+        pre_deposit=stmt.inserted.pre_deposit,
+        pre_monthly_rent=stmt.inserted.pre_monthly_rent,
+        sgg_cd=stmt.inserted.sgg_cd,
+        umd_nm=stmt.inserted.umd_nm,
+        use_rrright=stmt.inserted.use_rrright
     )
     with engine.begin() as conn:
         conn.execute(update_stmt)
--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String, Text, UniqueConstraint, Date
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql.sqltypes import Float
 
 Base = declarative_base()
 
@@ -23,3 +24,27 @@ class RegionCode(Base):
     __table_args__ = (
         UniqueConstraint("region_cd", name="uq_region_cd"),
     )
+
+class RealEstateDeal(Base):
+    __tablename__ = "real_estate_deals"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    build_year = Column(Integer)
+    contract_start = Column(String(10))
+    contract_end = Column(String(10))
+    contract_type = Column(String(10))
+    deal_day = Column(Integer)
+    deal_month = Column(Integer)
+    deal_year = Column(Integer)
+    deposit = Column(Integer)
+    exclu_use_ar = Column(Float)
+    floor = Column(Integer)
+    house_type = Column(String(20))
+    jibun = Column(String(20))
+    mhouse_nm = Column(String(50))
+    monthly_rent = Column(Integer)
+    pre_deposit = Column(String(20))
+    pre_monthly_rent = Column(String(20))
+    sgg_cd = Column(String(10))
+    umd_nm = Column(String(20))
+    use_rrright = Column(String(20))
